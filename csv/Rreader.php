bd166c9c08096902b3a1b9b509ec8f09b94c65cb
socialist <admin@socialist.pw>
2015-11-28 16:36:46 +0300

    Added some formats

---
+++ csv/Rreader.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace walk\exelreader\csv;
+
+use walk\exelreader\ExelReaderTypeInterface;
+
+/**
+* 
+*/
+class Reader implements ExelReaderTypeInterface
+{
+
+	public function getNumSheets()
+	{
+
+	}
+
+	
+	public function getSheetName($sheetIndex)
+	{
+
+	}
+
+	
+	public function getNumRows($sheetIndex = 0)
+	{
+		
+	}
+
+	
+	public function getNumCols($sheetIndex = 0)
+	{
+		
+	}
+
+	
+	public function getValue($row, $col, $sheet = 0)
+	{
+		
+	}
+
+	
+	public function getRows($sheetIndex)
+	{
+		
+	}
+
+	
+	public function getRow($rowNum, $sheetIndex)
+	{
+		
+	}
+	
+}
---
+++ xml/Reader.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace walk\exelreader\xml;
+
+use walk\exelreader\ExelReaderTypeInterface;
+
+/**
+* 
+*/
+class Reader implements ExelReaderTypeInterface
+{
+
+	public function getNumSheets()
+	{
+
+	}
+
+	
+	public function getSheetName($sheetIndex)
+	{
+
+	}
+
+	
+	public function getNumRows($sheetIndex = 0)
+	{
+		
+	}
+
+	
+	public function getNumCols($sheetIndex = 0)
+	{
+		
+	}
+
+	
+	public function getValue($row, $col, $sheet = 0)
+	{
+		
+	}
+
+	
+	public function getRows($sheetIndex)
+	{
+		
+	}
+
+	
+	public function getRow($rowNum, $sheetIndex)
+	{
+		
+	}
+}
